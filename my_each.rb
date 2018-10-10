4b8d299b73471c1efa6c13484546af6ff4b7964f
Ashleigh Chun <ashleigh.chun@wework.com>
2018-10-10 23:56:35 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,13 @@
 def my_each(array) # put argument(s) here
+  if block_given?
   while i < array.length 
     yield array[i]
+    i += 1
   end
+  array 
+  else puts "Hey! No block was given!"
 end
+
 
  my_each(words) do |word|
          puts word
