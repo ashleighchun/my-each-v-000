2697e57538a3c320ce4bf2990f9763633c4ea513
Ashleigh Chun <ashleigh.chun@wework.com>
2018-10-11 05:00:16 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -5,7 +5,7 @@ def my_each(array) # put argument(s) here
   while i < array.length 
     yield array[i]
     i = i + 1
-    binding.pry
+    #binding.pry
   end
   array 
   else puts "Hey! No block was given!"
